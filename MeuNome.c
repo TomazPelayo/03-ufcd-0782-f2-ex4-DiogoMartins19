9fc187fe646827907ffe03cd2b066f9a33fce662
DiogoMartins19 <122288968+DiogoMartins19@users.noreply.github.com>
2023-01-23 14:11:38 +0000

    Update MeuNome.c

--- MeuNome.c
+++ MeuNome.c
@@ -1,3 +1,12 @@
 // Resolva neste ficheiro o seu exercício
 
+#include<stdio.h>
 
+    int main() {
+
+printf(“Nome : Carlos Fonseca
+Data : 14 de Setembro de 2007
+Morada : Santo Tirso \n");
+
+        return(0);
+    }
